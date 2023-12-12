8e19e4a749393d5b7f37a58d2cf055224e0c6a54
Ilya Ginzburg <ilya_g@rad.com>
2023-12-12 15:41:20 +0200

    EcoPolling

--- EcoCheck/EcoPolling.tcl
+++ EcoCheck/EcoPolling.tcl
@@ -1,10 +1,18 @@
+package require sqlite3
+set db_file     //prod-svm1/tds/Temp/SQLiteDB/EcoCheck.db
 set ecoFilePath //prod-svm1/tds/Temp/SQLiteDB/EcoNoiNpi
+package require json
+console show
+
 
 proc MainEcoPolling {} {
   set ret [ReadEcoFiles]
   
   return $ret
 }
+# ***************************************************************************
+# ReadEcoFiles
+# ***************************************************************************
 proc ReadEcoFiles {} {
   if ![file exists $::ecoFilePath] {
     tk_messageBox -type ok -title "No path" -message "The \'$::ecoFilePath\' doesn't exist"
@@ -13,18 +21,33 @@ proc ReadEcoFiles {} {
     set ret 0
   }
   if {$ret==0} {
-    set ecoFiles [lsort -dict [glob -directory $::ecoFilePath \[CN\]*]]
+    set ecoFiles [lsort -dict [glob -nocomplain -directory $::ecoFilePath \[CN\]*]]
     if [llength $ecoFiles] {
       foreach ecoFile $ecoFiles {
-        set ret [ReadEcoFile $ecoFile]  
-        if {$ret!=0} {break}        
+        set ret [ReadEcoFile $ecoFile] 
+        puts "ret of ReadEcoFile: <$ret>"        
+        if {$ret!=0} {break}    
+        set ret [EcoData2DB $ecoFile]  
+        puts "ret of EcoData2DB: <$ret>"  
+        if {$ret=="emailAndDelete"} {
+          ## ECO was added to ReleasedNotApproved
+          set ret [SendEmail $ecoFile]   
+          puts "ret of SendEmail: <$ret>"            
+        }  
+        set ret [DeleteEcoFile $ecoFile]  
+        puts "ret of DeleteEcoFile: <$ret>"         
       }
     }
   }
   return $ret
 }
+# ***************************************************************************
+# ReadEcoFile
+# ***************************************************************************
 proc ReadEcoFile {ecoFile} {
+  puts "\nReadEcoFile $ecoFile"
   set ecoTail [file tail $ecoFile]
+  set ecoFileName [lindex [split $ecoTail .] 0]
   if [catch {open $ecoFile r+} id] {
     set txt "$id\n\nCan't open file \'$ecoFile\'"
     tk_messageBox -type ok -title "Can't open file \'$ecoTail\'" -message $txt
@@ -34,11 +57,104 @@ proc ReadEcoFile {ecoFile} {
     while {[gets $id line] >= 0 } {
       set line [string trim $line]
       if {[string length $line] !=0} {
-        lappend lines $line
+        append body $line
       }  
     }  
     close $id
-    puts "ecoFile:<$ecoFile> lines:<$lines>"    
+    #puts "ecoFile:<$ecoFile> body:<$body>" 
+    
+    set asadict [::json::json2dict $body]
+    foreach {name wotsit} $asadict {
+      set ::wit $wotsit
+      foreach {par val} [lindex $wotsit 0] {
+        set ::a${ecoFileName}($par) $val
+      }
+    }
+    parray ::a${ecoFileName}
   }
   return $ret
 }
+# ***************************************************************************
+# EcoData2DB
+# ***************************************************************************
+proc EcoData2DB {ecoFile} {
+  puts "\nEcoData2DB $ecoFile"
+  set ecoTail [file tail $ecoFile]
+  set ecoFileName [lindex [split $ecoTail .] 0]
+  set ecoUnits [lsort -dictionary [set ::a${ecoFileName}(AI)]]
+  puts "EcoData2DB ecoUnits:<$ecoUnits>"
+  set initsToReleasedNotApproved $ecoUnits
+    
+  sqlite3 dataBase $::db_file
+  dataBase timeout 5000
+  
+  # ## create list of products/units that mentioned in YZ' file, exist in ReleasedApproved and have ApprInAdv = 'no'
+  # ## such unit must be inseted to ReleasedNotApproved
+  # catch {dataBase eval {SELECT Unit from ReleasedApproved WHERE (ECO = $ecoFileName AND ApprInAdv = 'no')}} notAiAunits
+  # set notAiAunits [lsort -dictionary $notAiAunits]
+  # puts "notAiAunits:<$notAiAunits>"
+  
+  ## create list of products/units that mentioned in YZ' file, exist in ReleasedApproved and have ApprInAdv = 'yes'
+  ## all of units from this list will not insert to ReleasedNotApproved
+  catch {dataBase eval {SELECT Unit from ReleasedApproved WHERE (ECO = $ecoFileName AND ApprInAdv = 'yes')}} yesAiAunits
+  set yesAiAunits [lsort -dictionary $yesAiAunits]
+  puts "yesAiAunits:<$yesAiAunits>"
+  
+  if 0 {
+  set initsToReleasedNotApproved ""
+  foreach unit $ecoUnits {
+    if {[lsearch $notAiAunits $unit]!="-1"} {
+      lappend initsToReleasedNotApproved $unit
+    }    
+  }
+  foreach unit $ecoUnits {
+    if {[lsearch $yesAiAunits $unit]=="-1"} {
+      lappend initsToReleasedNotApproved $unit
+    }    
+  }
+  set initsToReleasedNotApproved [lsort -unique $initsToReleasedNotApproved]
+  }
+  foreach ecoUnit $ecoUnits {
+    foreach yesAiAunit $yesAiAunits {
+      if {$ecoUnit==$yesAiAunit} {
+        set indx [lsearch $ecoUnits $yesAiAunit]
+        set initsToReleasedNotApproved [lreplace $initsToReleasedNotApproved $indx $indx]
+      }
+    }
+  }  
+  puts "initsToReleasedNotApproved:<$initsToReleasedNotApproved>"
+  
+  foreach unit $initsToReleasedNotApproved {
+    set number [set ::a${ecoFileName}(number)]
+    set relDate [set ::a${ecoFileName}(releise_date)]
+    catch {dataBase eval {INSERT INTO ReleasedNotApproved VALUES($number,$unit,$relDate)}} res
+  }
+  dataBase close
+  
+  if [llength $initsToReleasedNotApproved] {
+    set ret emailAndDelete
+  } else {
+    set ret justDelete
+  }
+  return $ret 
+}  
+
+# ***************************************************************************
+# SendEmail
+# ***************************************************************************
+proc SendEmail {ecoFile} {
+  set ecoFileName [lindex [split [file tail $ecoFile] .] 0]
+  puts "\nSendEmail $ecoFileName"
+  return 0
+}
+# ***************************************************************************
+# DeleteEcoFile
+# ***************************************************************************
+proc DeleteEcoFile {ecoFile} {
+  set ecoFileNew _[file tail $ecoFile]
+  set ecoFilePathNew [file join [file dirname $ecoFile] $ecoFileNew]
+  puts "\nDeleteEcoFile $ecoFile $ecoFilePathNew"
+  ##file rename -force $ecoFile $ecoFilePathNew
+  return 0
+}
+
--- EcoCheck/staff.tcl
+++ EcoCheck/staff.tcl
@@ -29,3 +29,111 @@ proc GetDbrName {id_number} {
   
   return ok_$dbr_name
 }
+## RetriveIdTraceData DF100148093 MKTItem4Barcode
+## RetriveIdTraceData 21181408    PCBTraceabilityIDData
+proc RetriveIdTraceData {args} {
+  package require http
+  package require tls
+  package require base64
+  ::http::register https 8445 ::tls::socket
+  global gaSet
+  set gaSet(fail) ""
+  puts "RetriveIdTraceData $args"
+  set barc [format %.11s [lindex $args 0]]
+  
+  set command [lindex $args 1]
+  switch -exact -- $command {
+    CSLByBarcode          {set barcode $barc  ; set traceabilityID null}
+    PCBTraceabilityIDData {set barcode null   ; set traceabilityID $barc}
+    MKTItem4Barcode       {set barcode $barc  ; set traceabilityID null}
+    OperationItem4Barcode {set barcode $barc  ; set traceabilityID null}
+    default {set gaSet(fail) "Wrong command: \'$command\'"; return -1}
+  }
+  set url "https://ws-proxy01.rad.com:8445/ATE_WS/ws/rest/"
+  set param [set command]\?barcode=[set barcode]\&traceabilityID=[set traceabilityID]
+  append url $param
+  puts "url:<$url>"
+  set tok [::http::geturl $url -headers [list Authorization "Basic [base64::encode webservices:radexternal]"]]
+  update
+  set st [::http::status $tok]
+  set nc [::http::ncode $tok]
+  if {$st=="ok" && $nc=="200"} {
+    #puts "Get $command from $barc done successfully"
+  } else {
+    set gaSet(fail) "http::status: <$st> http::ncode: <$nc>"; return -1
+  }
+  upvar #0 $tok state
+  #parray state
+  puts "$state(body)"
+  set body $state(body)
+  set jbody $state(body)
+  ::http::cleanup $tok
+  
+  set re {[{}\[\]\,\t\:\"]}
+  set tt [regsub -all $re $body " "]
+  set ret [regsub -all {\s+}  $tt " "]
+  set rett [lindex $ret end]
+  puts ""
+  set asadict [::json::json2dict $jbody]
+  foreach {name wotsit} $asadict {
+    set ::wit $wotsit
+    puts "name:<$name> wotsit:<$wotsit>"
+    foreach {par val} [lindex $wotsit 0] {
+      puts "name:<$name> par:<$par> val:<$val>"
+    }
+  }
+  # puts ""
+  # set asadict2 [::json::many-json2dict $jbody]
+  # foreach {name wotsit} $asadict2 {
+    # puts "name:<$name> val:<$wotsit>"
+  # }
+  return $rett
+}
+
+# ***************************************************************************
+# EcoData2DB
+# ***************************************************************************
+proc EcoData2DB {ecoFile} {
+  puts "\nEcoData2DB $ecoFile"
+  set ecoTail [file tail $ecoFile]
+  set ecoFileName [lindex [split $ecoTail .] 0]
+  
+  sqlite3 dataBase $::db_file
+  dataBase timeout 5000
+  #catch {dataBase eval {SELECT count(*) from ReleasedApproved WHERE ECO = $ecoFileName}} count
+  #puts "count:<$count>"
+  catch {dataBase eval {SELECT Unit from ReleasedApproved WHERE ECO = $ecoFileName}} units
+  set unitsLen [llength $units]
+  puts "units:<$units> unitsLen:<$unitsLen>"
+  
+  
+  set res res0
+  if {$unitsLen>0} {
+    catch {dataBase eval {SELECT ECO from ReleasedApproved WHERE ApprInAdv = \'yes\'}} res
+    if {$res==""} {
+      set res "nonAiA"
+    } else {
+      set res "AiA"
+    }
+    puts "res1:<$res>"    
+  }
+  
+  if {$unitsLen==0 || $res=="nonAiA"} {
+    puts "EcoData2DB unitsLen:<$unitsLen> res:<$res> units:<[set ::a${ecoFileName}(AI)]>"
+    foreach unit [set ::a${ecoFileName}(AI)] {
+      set number [set ::a${ecoFileName}(number)]
+      set relDate [set ::a${ecoFileName}(releise_date)]
+      catch {dataBase eval {INSERT INTO ReleasedNotApproved VALUES($number,$unit,$relDate)}} res
+    }
+    
+    if {$res==""} {
+      set res emailAndDelete
+      puts "res2:<$res>"
+    }
+  } else {
+    set res justDelete
+  }
+  dataBase close
+  
+  return $res  
+}
